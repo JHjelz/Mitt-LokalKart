384c942ca637211e0cb7ae3832b4992cc53b9825
jshjelse <jshjelse@stud.ntnu.no>
2026-03-04 22:01:04 +0100

    index.html fungerer med kart

---
+++ js/map.js
@@ -0,0 +1,14 @@
+// MITT-LOKALKART/js/map.js
+
+// Opprett kartet
+window.map = L.map('map', {zoomControl: false}).setView([60.1659969, 10.2559499], 14);
+
+// Legger til zoom
+L.control.zoom({position: 'bottomright'}).addTo(map);
+
+// Legg til bakgrunnskart (OpenStreetMap)
+const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+});
+
+osmLayer.addTo(map);
